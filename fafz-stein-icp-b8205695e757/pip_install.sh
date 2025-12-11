5a4e323926e75ae43037ffe3a23ef4133193cb79
kk6696kk <100404082+kk6696kk@users.noreply.github.com>
2025-12-11 16:07:38 +0800

    Delete fafz-stein-icp-b8205695e757 directory

--- fafz-stein-icp-b8205695e757/pip_install.sh
+++
@@ -1,8 +0,0 @@
-#TORCH="1.6.0"
-#CUDA="cu102"
-
-pip install -r requirements.txt
-#pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
-#pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
-#pip install torch-cluster==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
-#pip install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
